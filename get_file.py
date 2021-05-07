63ff6b500b94c8597591e52ca1d83ff46f58a393
Gerwin Smits <smitsg@ziggo.nl>
2021-05-07 12:36:34 +0200

    added comments in main and File class

--- get_file.py
+++ get_file.py
@@ -1,45 +1,22 @@
 import os
+from pathlib import Path
 
-# Class with functions for file handling
+""" ---------------------------------------------- 
+    Create Class with functions for file handling
+    ---------------------------------------------- 
+"""
 class File:
+  # init of Class
   def __init__(self, path):
-    self.__path = path
-
-  """ 
-  1. The following code contains getter and setter to define the path name.
-  If you want to add/delete a path, you can do this in a uniform way that's safe to use.
-  """
-  # return the path of your File object
-  @property
-  def path(self):
-    return self.__path
-  # set the path of your File object
-  @path.setter
-  def path(self, path):
-    self.__path = path
-  # delete the path of your File object
-  @path.deleter
-  def path(self):
-    print("deleter of x called")
-    del self.__path
-
-# Determine and return the basename, extracted from the path.
-  def get_basename_of_absolute_path(self, absolute_path):
-    basename = os.path.basename(absolute_path)
-    return(basename)
-# Determine and return the type of format (examples: .csv .fasta .fastq)
-  def get_extension_of_absolute_path(self, absolute_path):
-    name, extension = os.path.splitext(absolute_path)
-    return(name, extension)
-
-# check the type of format
-    def is_fasta(filename):
-      with open(filename, "r") as handle:
-          fasta = SeqIO.parse(handle, "fasta")
-          return any(fasta)  # False when `fasta` is empty, i.e. wasn't a FASTA file
+      self.__path = path
 
   def file_exist(self, absolute_path):
+    """------------------------------------------------
+       Check if the file exists
+       ------------------------------------------------
+    """
     try:
+      print(absolute_path)
       # Open the file for reading
       fileHandler = open(absolute_path)
       # Print the following message if no exception occurs
@@ -53,15 +30,40 @@ class File:
       # print the termination message
       print("End of the program")
 
+  """ ---------------------------------------------- 
+      Getters and Setters
+      The following code contains getter and setter to define the path name.
+      If you want to add/delete a path, you can do this in a uniform way that's safe to use.
+      ---------------------------------------------- 
+  """
+  @property
+  def path(self):
+    # return the path of your File object
+    return self.__path
 
+  @path.setter
+  def path(self, path):
+    # set the path of your File object
+    self.__path = path
 
+  @path.deleter
+  def path(self):
+    # delete the path of your File object
+    print("deleter of x called")
+    del self.__path
 
-  # @property
-  # def abs_path(self):
-  #    print("setter of absolute path called")
-  #    self._x = self.abs_path
-  #    return self._x
-  # @abs_path.setter
-  # def abs_path(self, abs_path):
-  #    print("getter of x called")
-  #    return self.abs_path
+  def get_basename_of_absolute_path(self, absolute_path):
+    # Determine and return the basename, extracted from the path.
+    basename = os.path.basename(absolute_path)
+    return(basename)
+
+  def get_extension_of_absolute_path(self, absolute_path):
+    # Determine and return the type of format (examples: .csv .fasta .fastq)
+    name, extension = os.path.splitext(absolute_path)
+    return(name, extension)
+
+  def is_fasta(absolute_path):
+    # check the type of format
+    with open(absolute_path, "r") as handle:
+        fasta = SeqIO.parse(handle, "fasta")
+        return any(fasta)  # False when `fasta` is empty, i.e. wasn't a FASTA file
--- main.py
+++ main.py
@@ -3,15 +3,35 @@ import os
 
 # The main
 def main():
-  # give your absolute path
-  absolute_path = "C:/Users/gerwi/OneDrive/Documents/example.csv" # Obviously not FASTA
-
-  """ Step 1. create an object my_file of the File Class in get_file.py.
-      You will give your absolute_path, which can be changed later.
+  """ ---------------------------------------------- 
+      Step 1. give your absolute path
+      ----------------------------------------------
   """  
+  absolute_path = "C:/Users/gerwi/OneDrive/Documents/GitHub/simple-bi-commands/GCF_001272835.1_ASM127283v1_genomic.fna" # Obviously not FASTA
+
+  """ ----------------------------------------------
+      Step 2. create an object my_file of the File Class in get_file.py.
+      You will give your absolute_path, which can be changed later.
+      -----------------------------------------------
+  """ 
   my_file = File(absolute_path)
-  # step 2. get basename of the path
+
+  """ -----------------------------------------------
+    Step 3. Check if the file on the absolute file path exist, otherwise you can quit the program immidately.
+      ------------------------------------------------
+  """
+  my_file.file_exist(my_file.path)
+
+  """ -----------------------------------------------
+    Step 4. Get the basename of the absolute path
+    -----------------------------------------------
+  """
   basename = my_file.get_basename_of_absolute_path(my_file.path)
+
+  """ -----------------------------------------------
+    Step 5. Get the file extension of the absolute path
+    -----------------------------------------------
+  """
   name, extension = my_file.get_extension_of_absolute_path(my_file.path)
   
 main()
